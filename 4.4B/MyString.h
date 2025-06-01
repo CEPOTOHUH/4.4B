605bd2bc34556f5c10d029037b5171cfffab3cce
srocn <srocn@DESKTOP-5M12MKV>
2025-06-01 13:58:23 +0300

    Добавьте файлы проекта.

---
+++ 4.4B/MyString.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <iostream>
+
+#include <utility>
+
+
+
+class MyString {
+
+private:
+
+	char* data;
+
+	size_t length;
+
+
+
+	bool isSpace(char c) const;
+
+	void copyFrom(const MyString& other);
+
+	void moveFrom(MyString&& other);
+
+	void free();
+
+
+
+public:
+
+	MyString();
+
+	MyString(const char* str);
+
+	MyString(const MyString& other);
+
+	MyString(MyString&& other) noexcept;
+
+	MyString& operator=(const MyString& other);
+
+	MyString& operator=(MyString&& other) noexcept;
+
+	~MyString();
+
+
+
+	MyString& operator+=(std::pair<char, size_t> p);
+
+	MyString& operator--();
+
+	MyString operator+(const MyString& other) const;
+
+
+
+	friend std::ostream& operator<<(std::ostream& out, const MyString& str);
+
+	friend std::istream& operator>>(std::istream& in, MyString& str);
+
+};
